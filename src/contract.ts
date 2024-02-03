8d08b7434cf429edbcdcf7289870b69d61255429
garageinc <9-b-rinat@rambler.ru>
2024-02-03 22:01:55 +0300

    :sparkles: (feat): V7, new main contract

--- src/contract.ts
+++ src/contract.ts
@@ -1,5 +1,5 @@
 
-export const BINGO_RINGO = '0x8c25b3808C206C4e9D00bAb7DB38B857bEDe8cec'.toLowerCase()
+export const BINGO_RINGO = '0x133B0DF2019113e7D44F5fE170dc839F54A13331'.toLowerCase()
 export const GAME_1 = '0x58729a1f5d408073ec5634ce216cfdf3553085d6'.toLowerCase()
 export const GAME_2 = '0x63383f3dec9c766d23c11c21da81a4b93dcdaea0'.toLowerCase()
 export const GAME_3 = '0x93372f672e85766ef1482b7ee3b5ac296d352de6'.toLowerCase()
